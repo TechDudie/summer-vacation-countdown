bbe44798f5350f19e75028e9e0979d4d98905640
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-28 12:44:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-
+var date = new Date(); // Current Date
+var vacation = new Date("06/01/2021"); // That's the summer vacation date for me, i'll make it customizable later
+var difference = vacation.getTime() - date.getTime();
+// TODO
